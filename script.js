5a207a8d6b837828003a5837e7ba2e2c8788d80e
rlmiguel <132916177+rl-miguel@users.noreply.github.com>
2024-01-24 23:57:24 +0000

    Create computer decision

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("test")
+
+function computerDecision(){
+    let initialValue=Math.random()*100;
+    console.log(`Computer has rolled ${initialValue}`);
+
+    if(initialValue <= 33.3){
+        initialValue="Rock";
+
+    } else if (initialValue <= 66.6){
+            initialValue="Paper";
+        } else {
+            initialValue="Scissors";
+        }
+        console.log(initialValue);
+    }
+
+
+
+computerDecision()
